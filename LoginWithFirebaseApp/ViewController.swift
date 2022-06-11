b5f58a687d00a9e0e7c7613a57773c549a044aa5
sugurunnba <sugurunnba7@gmail.com>
2022-06-11 21:15:11 +0900

    Ep.1 is finish

---
+++ LoginWithFirebaseApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LoginWithFirebaseApp
+//
+//  Created by 高木克 on 2022/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resisterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        resisterButton.layer.cornerRadius = 10
+    }
+
+
+}
+
